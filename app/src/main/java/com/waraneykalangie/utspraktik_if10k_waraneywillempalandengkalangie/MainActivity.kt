3d0d016b437fe79b521ca1d808483fa98009021b
wkalangie <wkalangie@gmail.com>
2023-05-30 21:00:26 +0700

    first commit

---
+++ app/src/main/java/com/waraneykalangie/utspraktik_if10k_waraneywillempalandengkalangie/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.waraneykalangie.utspraktik_if10k_waraneywillempalandengkalangie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
